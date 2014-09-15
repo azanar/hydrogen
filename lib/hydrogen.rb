130fe24d52d3172c50e9f0ff16fc1571585607a1
Ed Carrel <edward@carrel.org>
2014-09-15 03:47:18 -0700

    Hydrogen wants to know nothing about AWS.

--- lib/hydrogen.rb
+++ lib/hydrogen.rb
@@ -2,7 +2,6 @@ require "hydrogen/model"
 require "hydrogen/table_object"
 require "hydrogen/table_object/part"
 require "hydrogen/table_object/part/sequence"
-require "hydrogen/aws/table_object"
 require "hydrogen/version"
 
 module Hydrogen
