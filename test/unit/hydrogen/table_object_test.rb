4d55f58a37e6c45450007b66eb578c3ae89ea6d4
Ed Carrel <edward@carrel.org>
2014-10-08 00:38:53 -0700

    Test fixing!

--- test/unit/hydrogen/table_object_test.rb
+++ test/unit/hydrogen/table_object_test.rb
@@ -8,14 +8,12 @@ class Hydrogen::TableObjectTest < Test::Unit::TestCase
     @mock_table_object = mock
     @mock_table_object_name = "mock_table_name"
 
-    #@mock_table_object.expects(:name).returns(@mock_table_object_name)
-
+    @mock_table_object.expects(:kind_of?).with(Hydrogen::Model).returns(true)
     uri = URI("file:///tmp/foo")
 
     @table_object = Hydrogen::TableObject.new(@mock_table_object, uri)
   end
   test "#url" do
-    #@mock_table_object.expects(:name).twice.returns(@mock_table_object_name)
     @table_object.url
   end
 end
